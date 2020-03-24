d8ec2d52d5f7a86ba5cdaa08d508ebbc0d090763
Lucas Prieto de Senna <lucas-prieto@hotmail.com>
2020-03-24 11:56:43 -0300

    Modificação da API

--- src/main/java/rd/ecommerce/controller/ClientController.java
+++ src/main/java/rd/ecommerce/controller/ClientController.java
@@ -1,13 +1,15 @@
 package rd.ecommerce.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import rd.ecommerce.model.Cliente;
 import rd.ecommerce.repository.ClienteRepository;
 
+import javax.servlet.http.PushBuilder;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ClientController {
@@ -19,5 +21,28 @@ public class ClientController {
     public ResponseEntity <List<Cliente>> listar(){
         List<Cliente> clientes = repository.findAll();
         return ResponseEntity.ok().body(clientes);
+    }
+
+    @PostMapping("/clientes")
+    public ResponseEntity<?> adicionar(@RequestBody Cliente cliente){
+        if(cliente == null) {
+            return ResponseEntity.badRequest().body("Cliente não pode estar vazio");
+        }
+        return new ResponseEntity(repository.save(cliente), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/clientes/{id}")
+    public ResponseEntity<?> mostrar(@PathVariable("id") Long id){
+        return ResponseEntity.ok().body(repository.findById(id));
+    }
+
+    @GetMapping("/clientes2/{id}")
+    public ResponseEntity<?> mostrar2(@PathVariable("id") Long id){
+        Optional<Cliente> opt_cliente = repository.findById(id);
+        Cliente cliente = opt_cliente.orElse(null);
+        if (cliente == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(cliente);
     }
 }
